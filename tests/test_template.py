ca764964bfa13f3ec312bac5284c706675ff96a1
Patrick Schleiter <4884221+pschleiter@users.noreply.github.com>
2023-07-19 10:33:31 +0200

    Extends tests

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,24 +1,17 @@
 import pytest
 import json
-from flask import render_template_string
+
 import plotly
+from dash import html
 
-from dash_template_rendering import render_dash_template_string
-
-
-@pytest.mark.usefixtures("client")
-def test_empty_template():
-    with pytest.raises(
-        ValueError,
-        match="Empty template in use. Please remove.",
-    ):
-        render_dash_template_string(source="")
+from dash_template_rendering import render_dash_template, render_dash_template_string
 
 
 @pytest.mark.usefixtures("client")
-def test_template(template_string, dash_row, template_json):
-    template = render_dash_template_string(
-        source=render_template_string(template_string, dash_row=dash_row)
+def test_render_template(app, template_string, dash_row, template_json):
+    template = render_dash_template(
+        template_name_or_list=app.jinja_env.from_string(template_string),
+        dash_row=dash_row,
     )
 
     assert (
@@ -35,3 +28,49 @@ def test_render_dash_template_string(template_string, dash_row, template_json):
         json.loads(plotly.io.json.to_json_plotly(template.to_plotly_json()))
         == template_json
     )
+
+
+@pytest.mark.usefixtures("client")
+def test_empty_template():
+    with pytest.raises(
+        ValueError,
+        match="Empty template in use. Please remove.",
+    ):
+        render_dash_template_string(source="")
+
+
+@pytest.mark.usefixtures("client")
+def test_two_main_tags_warning_template():
+    with pytest.warns(
+        UserWarning,
+        match=(
+            "Template Tag has more than one main tag, "
+            "which is not supported. "
+            "Only the first tag is used."
+        ),
+    ):
+        template = render_dash_template_string(
+            source="""
+                <div id="tag1"></div>
+                <div id="tag2"></div>
+            """
+        )
+
+    assert isinstance(template, html.Div)
+    assert template.id == "tag1"
+
+
+@pytest.mark.usefixtures("client")
+def test_skip_unknown_tags_template():
+    with pytest.raises(
+        TypeError,
+        match=(
+            "Generating dash component from html tag failed. "
+            'No corresponding dash component found for html tag "unknown_tag".'
+        ),
+    ):
+        render_dash_template_string(
+            source="""
+                <unknown_tag></unknown_tag>
+            """
+        )
