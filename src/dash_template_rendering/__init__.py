26a53b77d262ca7816209a59660f3cf873030f42
Patrick Schleiter <4884221+pschleiter@users.noreply.github.com>
2023-07-19 23:06:05 +0200

    Fix flake8 failuer

--- src/dash_template_rendering/__init__.py
+++ src/dash_template_rendering/__init__.py
@@ -3,7 +3,7 @@ dash_template_rendering is a simple Dash Python extension for rendering html con
 for the dashboard with jinja2. The rendered content can be used for the ``dash.layout``
 and the return of callbacks.
 """
-from .template_renderer import TemplateRenderer
-from .templating import render_dash_template, render_dash_template_string
+from .template_renderer import TemplateRenderer  # noqa: F401
+from .templating import render_dash_template, render_dash_template_string  # noqa: F401
 
 __version__ = "0.0.1-beta1"
